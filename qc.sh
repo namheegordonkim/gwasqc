fa19fca1bb5f3debde82d0c0e4201c52f20d285e
Nam Hee Gordon Kim <namheegordonkim@ip139.cmmt.ubc.ca>
2017-05-03 15:02:46 -0700

    conversion for PCA

--- qc.sh
+++ qc.sh
@@ -8,7 +8,7 @@ data=$1
 out=$2
 
 # step 1: exclude uncertains
-plink --noweb --bfile $data --prune --make-bed --out $out
+# do nothing--there is no known phenotype associated with the data
 
 # step 2: update/check FAM with AD status
 # no idea how to do this...
@@ -34,4 +34,33 @@ plink --noweb --bfile $out --genome --min 0.05 --out $out
 # remove relateds
 plink --noweb --bfile $out --rel-ctoff --out $out
 
-# step 7: EIGENSTRAT
+# step 7: PCA by EIGENSTRAT
+# requires system to have set EIGPATH pointing to eigsoft's bin folder
+
+# step 7a: PACKEDPED -> PACKEDANCESTRYMAP
+echo genotypename:    $out.bed > $EIGPATH/par.PACKEDPED.EIGENSTRAT
+echo snpname:         $out.bim >> $EIGPATH/par.PACKEDPED.EIGENSTRAT # or example.map, either works
+echo indivname:       $out.fam >> $EIGPATH/par.PACKEDPED.EIGENSTRAT # or example.ped, either works
+echo outputformat:    PACKEDANCESTRYMAP >> $EIGPATH/par.PACKEDPED.EIGENSTRAT
+echo genotypeoutname: $out.packedancestrymapgeno >> $EIGPATH/par.PACKEDPED.EIGENSTRAT
+echo snpoutname:      $out.snp >> $EIGPATH/par.PACKEDPED.EIGENSTRAT
+echo indivoutname:    $out.ind >> $EIGPATH/par.PACKEDPED.EIGENSTRAT
+echo familynames:     NO >> $EIGPATH/par.PACKEDPED.EIGENSTRAT
+
+# step 7b: PACKEDANCESTRYMAP -> ANCESTRYMAP
+echo genotypename:    $out.packedancestrymapgeno > $EIGPATH/par.PACKEDANCESTRYMAP.ANCESTRYMAP
+echo snpname:         $out.snp >> $EIGPATH/par.PACKEDANCESTRYMAP.ANCESTRYMAP
+echo indivname:       $out.ind >> $EIGPATH/par.PACKEDANCESTRYMAP.ANCESTRYMAP
+echo outputformat:    ANCESTRYMAP >> $EIGPATH/par.PACKEDANCESTRYMAP.ANCESTRYMAP
+echo genotypeoutname: $out.ancestrymapgeno >> $EIGPATH/par.PACKEDANCESTRYMAP.ANCESTRYMAP
+echo snpoutname:      $out.snp >> $EIGPATH/par.PACKEDANCESTRYMAP.ANCESTRYMAP
+echo indivoutname:    $out.ind >> $EIGPATH/par.PACKEDANCESTRYMAP.ANCESTRYMAP
+
+# step 7c: ANCESTRYMAP -> EIGENSTRAT
+echo genotypename:    $out.ancestrymapgeno > $EIGPATH/par.ANCESTRYMAP.EIGENSTRAT
+echo snpname:         $out.snp >> $EIGPATH/par.ANCESTRYMAP.EIGENSTRAT
+echo indivname:       $out.ind >> $EIGPATH/par.ANCESTRYMAP.EIGENSTRAT
+echo outputformat:    EIGENSTRAT >> $EIGPATH/par.ANCESTRYMAP.EIGENSTRAT
+echo genotypeoutname: $out.eigenstratgeno >> $EIGPATH/par.ANCESTRYMAP.EIGENSTRAT
+echo snpoutname:      $out.snp >> $EIGPATH/par.ANCESTRYMAP.EIGENSTRAT
+echo indivoutname:    $out.ind >> $EIGPATH/par.ANCESTRYMAP.EIGENSTRAT
