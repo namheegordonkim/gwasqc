487ab4ffb39e9bb4f5e8868c157ef64e12643c94
Nam Hee Gordon Kim <namheegordonkim@ip139.cmmt.ubc.ca>
2017-05-03 14:08:05 -0700

    created procedure up to step 6

--- qc.sh
+++ qc.sh
@@ -8,14 +8,30 @@ data=$1
 out=$2
 
 # step 1: exclude uncertains
-plink --file $data --prune --make-bed --out $out
+plink --noweb --file $data --prune --make-bed --out $out
 
 # step 2: update/check FAM with AD status
 # no idea how to do this...
 
 # step 3: sex check on X chromosome
-plink --bfile $out --check-sex --out $out # generates .sexcheck file
+plink --noweb --bfile $out --check-sex --out $out # generates .sexcheck file
 # exclude non-OKs
-plink --bfile $out --exclude $out.sexcheck
+plink --noweb --bfile $out --exclude $out.sexcheck --make-bed --out $out
 
-#
+# step 4: remove sex chromosomes and mtDNA from SNP arrays
+plink --noweb --bfile $out --chr 1-22 --make-bed --out $out
+
+# step 5: heterozygosity statistics
+plink --noweb --bfile $out --het  --out $out
+
+# step 5: SNP pruning by LD (specified in the doc)
+plink --noweb --bfile $out --indep-pairwise 1500 150 0.1 --out $out
+plink --noweb --bfile $out --extract $out.prune.in --recode --mind 0.1 --make-bed --out $out
+
+# step 6: cryptic relatedness/IBD check
+# IBD check (make .genome file)
+plink --noweb --bfile $out --genome --min 0.05 --out $out
+# remove relateds
+plink --noweb --bfile $out --rel-ctoff --out $out
+
+# step 7: EIGENSTRAT 
