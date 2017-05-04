30de189ffbc034f2128ff8be473aef29515870f2
Nam Hee Gordon Kim <namheegordonkim@ip139.cmmt.ubc.ca>
2017-05-04 13:23:10 -0700

    file structure refined

--- scripts/replace_uncertains_fam.R
+++ scripts/replace_uncertains_fam.R
@@ -1,8 +1,8 @@
 # based on https://www.cog-genomics.org/plink2/formats#fam
 
 args <- commandArgs(TRUE)
-infile <- args[1]
-outfile <- args[2]
+infile <- paste(args[1], ".fam", sep="")
+outfile <- paste(args[2], ".fam", sep="")
 
 fam <- read.table(infile)
 fam$V6 <- rep(1, nrow(fam))
