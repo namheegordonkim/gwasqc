7ecadeb01236177b7b383134fe290471b236a536
Nam Hee Kim <nkim412@head.hpc.local>
2017-05-26 18:11:49 -0700

    isolated LD-based pruning step

--- qc.sh
+++ qc.sh
@@ -40,6 +40,7 @@ echo "Updating gender information"
 Rscript scripts/update_sexinfo.R $tmp1.fam $subinfo $tmp1.fam
 
 plink --noweb --bfile $tmp1 --check-sex --out $out # generates .sexcheck file
+plink --bfile $tmp1 --remove $out.nosex --make-bed --out $tmp2  # removes ambiguous individuals
 
 # step 4: remove sex chromosomes and mtDNA from SNP arrays
 plink --noweb --bfile $tmp2 --chr 1-22 --make-bed --out $tmp1
@@ -47,39 +48,35 @@ plink --noweb --bfile $tmp2 --chr 1-22 --make-bed --out $tmp1
 # step 5: heterozygosity statistics
 plink --noweb --bfile $tmp1 --het --out $out
 
+# === LD-Based Pruning and PCA steps (don't use for imputation) ===
+
 # step 5: SNP pruning by LD (specified in the doc)
 plink --noweb --bfile $tmp1 --indep-pairwise 1500 150 0.1 --out $out
-plink --noweb --bfile $tmp1 --extract $out.prune.in --mind 0.1 --make-bed --out $tmp2
+plink --noweb --bfile $tmp1 --extract $out.prune.in --mind 0.1 --make-bed --out $tmp2.pruned
 
 # step 6: cryptic relatedness/IBD check
 # IBD check (make .genome file)
-plink --noweb --bfile $tmp2 --genome --min 0.05 --make-bed --out $tmp1
+plink --noweb --bfile $tmp2.pruned --genome --min 0.05 --make-bed --out $tmp1.pruned
 # remove relateds
-plink --noweb --bfile $tmp1 --rel-cutoff --make-bed --out $tmp2
+plink --noweb --bfile $tmp1.pruned --rel-cutoff --make-bed --out $tmp2.pruned
 
 # step 7: PCA by EIGENSTRAT
-# echo genotypename:    $out.ped > ./par.PED.EIGENSTRAT
-# echo snpname:         $out.map >> ./par.PED.EIGENSTRAT
-# echo indivname:       $out.fam >> ./par.PED.EIGENSTRAT
-# echo outputformat:    EIGENSTRAT >> ./par.PED.EIGENSTRAT
-# echo genotypeoutname: $out.eigenstratgeno >> ./par.PED.EIGENSTRAT
-# echo snpoutname:      $out.snp >> ./par.PED.EIGENSTRAT
-# echo indivoutname:    $out.ind >> ./par.PED.EIGENSTRAT
-# echo familynames:     NO >> ./par.PED.EIGENSTRAT
-#
-# convertf -p ./par.PED.EIGENSTRAT
 
 # coming back to step 2, mask all the -9s
-Rscript scripts/replace_uncertains_fam.R $tmp2 $tmp2
-
-# recode
-plink --noweb --bfile $tmp2 --recode --out $tmp2
-
-smartpca.perl -i $tmp2.ped -a $tmp2.map -b $tmp2.fam -s 6 \
--e $out.eval -l $out.elog -o $out.pca -p $out.plot
+Rscript scripts/replace_uncertains_fam.R $tmp2.pruned $tmp2.pruned
 
 # step 7b: remove outliers/duplicates from the pruned file
-# plink --noweb --list-duplicate-vars
+plink --noweb --bfile $tmp2.pruned --list-duplicate-vars ids-only suppress-first --out $tmp2.pruned
+
+plink --noweb --bfile $tmp2.pruned --remove $tmp2.pruned.dupvar --make-bed --out $tmp1.pruned
+
+# recode
+plink --noweb --bfile $tmp1.pruned --recode --out $tmp1.pruned
+
+smartpca.perl -i $tmp1.pruned.ped -a $tmp1.pruned.map -b $tmp1.pruned.fam -s 6 \
+-e $out.eval -l $out.elog -o $out.pca -p $out.plot
+
+# === End of PCA Steps ===
 
 # step 8: MAF, HWE, MND, GENO
 plink --noweb --bfile $tmp2 --maf 0.01 --hwe 0.000005 --mind 0.05 --geno 0.05 --make-bed --out $out
